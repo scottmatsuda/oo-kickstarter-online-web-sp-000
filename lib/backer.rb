2bcce26b6f595511db403f078d0c841afb217999
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-21 20:32:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,12 +4,12 @@ class Backer
   
   def initialize(name)
     @name = name
-    @@backed_projects = []
+    @backed_projects = []
   end
   
   def back_project(project)
     backed_projects << project
-    @@backers
+    @backers
   end
   
 end
